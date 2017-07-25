41c79d4f825e20c32dd3d59bec9657ab2bacbfeb
vincylin <995413628@qq.com>
2017-07-25 11:22:19 +0800

    tab修改

--- shuilisite/newStatic/js/index.js
+++ shuilisite/newStatic/js/index.js
@@ -3,13 +3,26 @@ $(function(){
     $('#header').load('header.html');
     $('#footer').load('footer.html');
 
-    //tab切换
-    $('.tab-item').click(function () {
+    //左边tab切换
+    $('.tab-item').click(function (e) {
+        var id = e.currentTarget.id;
+        console.log(e.currentTarget.id);
         $(this).addClass('active').siblings().removeClass('active');
+        $('.' + id).addClass('active').siblings().removeClass('active');
+
+    });
+    //sub-tab切换
+    $('.sub-tab-item').click(function (e) {
+        var id = e.currentTarget.id;
+        $(this).addClass('active').siblings().removeClass('active');
+        $('.' + id).addClass('current').siblings().removeClass('current');
     });
     //底部tab切换
-    $('.order-nav-item').click(function () {
+    $('.order-nav-item').click(function (e) {
+        var id = e.currentTarget.id;
         $(this).addClass('current').siblings().removeClass('current');
+        $('.' + id).addClass('active').siblings().removeClass('active');
+
     });
 
     $('.carousel').carousel({
